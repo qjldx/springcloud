c7fbdc42e34b4644648ccbdd25c912902cc58c96
qjldx <qjldx@126.com>
2020-12-08 00:46:14 +0800

    first commit

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,10 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+         SpringBootApplication.run(PaymentMain8001.class,args);
+    }
+}
