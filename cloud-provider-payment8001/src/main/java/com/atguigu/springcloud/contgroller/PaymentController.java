6a0bdaac5e82f6cb436fc282cda9361013746b4f
qjldx <qjldx@126.com>
2020-12-08 23:34:08 +0800

    first commit corect

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/contgroller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/contgroller/PaymentController.java
@@ -3,6 +3,10 @@ package com.atguigu.springcloud.contgroller;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 import javax.annotation.Resource;
@@ -13,25 +17,24 @@ public class PaymentController {
    @Resource
     private PaymentService paymentService;
 
-    @PostMapping(Value="/payment/create")
+    @PostMapping(value="/payment/create")
     public CommonResult create(Payment payment)
     {
       int result =paymentService.create(payment);
-        Log.info("插入的结果******"+result);
+        System.out.println("插入的结果******"+result);
         if (result>0){
             return new CommonResult(200,"插入数据成功",result);
         } else {
             return new CommonResult(400,"插入数据库失败");
         }
     }
-    @GetMapping(Value="/payment/{id}")
+    @GetMapping(value="/payment/{id}")
     public CommonResult getPaymentById(@PathVariable("id") Long id )
     {
         Payment payment = paymentService.getPaymentById(id);
 
 
-
-        Log.info("插入的结果******"+payment);
+        System.out.println("插入的结果******"+payment);
         if (payment!=null){
             return new CommonResult(200,"插入数据成功",payment);
         } else {
